d8c73ae3b2a42d18bd07823db29da689607cd416
Adarshaachar <109293703+Adarshaachar@users.noreply.github.com>
2025-02-04 00:39:08 +0530

    Add files via upload

---
+++ ASSIGNMENT1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace ASSIGNMENT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                conn = new SqlConnection("data source=ADARSHA\\SQLEXPRESS;database=AdventureWorks2019;Integrated Security=SSPI");
+                SqlCommand comm = new SqlCommand("CREATE TABLE MYTABLE3(Id INT PRIMARY KEY,Name VARCHAR(100),Email VARCHAR(50))", conn);
+                conn.Open();
+                comm.ExecuteNonQuery();
+                Console.WriteLine("Table Created Successfully");
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
