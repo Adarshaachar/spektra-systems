4af5217e65e456d88c08861a5eaa1f3ea7ce1309
Adarshaachar <109293703+Adarshaachar@users.noreply.github.com>
2025-02-04 00:15:43 +0530

    Add files via upload

---
+++ wordcount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+namespace wordcount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence:");
+            string str = Console.ReadLine().Trim();
+            int count = 0;
+            if (str.Length > 0)
+            {
+                count++;
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str[i] == ' ')
+                    {
+                        count++;
+
+                    }
+                }
+            }
+            Console.WriteLine("number of words:"+count);
+
+        }
+    }
+}
