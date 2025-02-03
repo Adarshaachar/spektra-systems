1329705757d7acfcbac94920735a550ded02b0fa
Adarshaachar <109293703+Adarshaachar@users.noreply.github.com>
2025-02-04 00:27:24 +0530

    Add files via upload

---
+++ Jsonassignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace Jsonassignment
+{
+    class product
+    {
+        public int PId;
+        public string PName;
+        public int Price;
+        public int Quantity;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<product> products = new List<product>();
+            Console.WriteLine("Enter number of product to be added:");
+            int n=Convert.ToInt32(Console.ReadLine());  
+            for(int i=0; i<n; i++)
+            {
+                Console.WriteLine("Details of "+(i+1)+" Product");
+                Console.Write("Enter the product Id:");
+                int id= Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter product Name:"); ;
+                string name= Console.ReadLine();
+                Console.Write("Enter the price of product:");
+                int price= Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the Quantity:");
+                int quantity= Convert.ToInt32(Console.ReadLine());  
+                products.Add( new product { PId = id,PName=name, Price = price, Quantity = quantity });
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Product details");
+            string json=JsonConvert.SerializeObject(products,Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
---
+++ TwoList/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace TwoList
+{
+    class student
+    {
+        public string name;
+        public int age;
+    }
+    class Class
+    {
+        public string name;
+        public string subject;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<student> students = new List<student>
+           {
+               new student {name="kiran",age=22},
+               new student {name="naveen",age=21},
+               new student {name="akhi",age=17},
+               new student {name="adarsha",age=19},
+               new student {name="bhargav",age=15},
+               new student {name="brunda",age=16}
+           };
+            List<Class> classes = new List<Class>
+            {
+                new Class {name="kiran",subject="Math"},
+                new Class {name="naveen",subject="CS"},
+                new Class {name="akhi",subject="ML"},
+                new Class {name="adarsha", subject="Fullstack"},
+                new Class {name="bhargav",subject="Math"},
+                new Class {name="brunda",subject="CS"}
+            };
+            var query = from student in students 
+                        where student.age > 18
+                        join classes_ in classes on student.name equals classes_.name 
+                        select new { student.name, classes_.subject}; 
+            Console.WriteLine("students whose age are above 18");
+            foreach(var names in query)
+            {
+                Console.WriteLine(names.name+" "+names.subject);
+            }
+           
+        }
+    }
+}
