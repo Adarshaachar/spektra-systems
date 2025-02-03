5b226350912e2d4d656d964e39f1c5abea5ddabe
Adarshaachar <109293703+Adarshaachar@users.noreply.github.com>
2025-02-04 00:46:07 +0530

    Add files via upload

---
+++ ASSIGNMENT2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.SqlClient;
+
+namespace ASSIGNMENT2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           SqlConnection con= null;
+            try
+            {
+                con = new SqlConnection("data source=ADARSHA\\SQLEXPRESS;database=AdventureWorks2019;integrated security=SSPI");
+                SqlCommand comm = new SqlCommand(@"SELECT DISTINCT CONCAT(A.FirstName,' ',A.MiddleName,' ',A.LastName) AS Employee_name from Person.Person A
+                                                   JOIN Sales.SalesOrderHeader B ON A.BusinessEntityID=B.SalesPersonID
+                                                   JOIN Sales.SalesOrderDetail C ON B.SalesOrderId=c.SalesOrderId
+                                                   JOIN Production.Product D ON C.productID=D.productID
+                                                   WHERE D.ProductNumber='BK-M18S-40'", con);
+                con.Open();
+               SqlDataReader read=comm.ExecuteReader();
+                while (read.Read()) {
+                    Console.WriteLine(read["Employee_name"]);                
+                }
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }finally
+            {
+                if (con != null) con.Close();
+            }
+        }
+    }
+}
---
+++ ASSIGNMENT3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.SqlClient;
+
+namespace ASSIGNMENT3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con=null;
+            try
+            {
+                con = new SqlConnection("Data Source =ADARSHA\\SQLEXPRESS; Initial Catalog = AdventureWorks2019; Integrated Security = SSPI");
+                Console.WriteLine("ENTER ID to fetch the data");
+                int id = Convert.ToInt32(Console.ReadLine());
+                SqlCommand comm = new SqlCommand("EXEC ProductRankLocation @id;", con);
+                comm.Parameters.AddWithValue("@id", id);
+                con.Open();
+                SqlDataReader reader=comm.ExecuteReader();
+                Console.WriteLine(new string('-', 100));
+                Console.WriteLine($"|{"ProductID",-20}| {"Name",-25}| {"Quantity",20}| {"Ranking",-20}|");
+                Console.WriteLine(new string('-', 100));
+                while (reader.Read())
+                {
+                    Console.WriteLine($"|{reader["ProductID"],-20}| {reader["Name"],-25}| {reader["Quantity"],20}| {reader["Ranking"],-20 }|");
+                }
+                Console.WriteLine(new string('-', 100));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }finally
+            {
+                if (con != null) con.Close();
+            }
+        }
+    }
+}
